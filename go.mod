a704e015523ca5f4f83e3087ea0a141d11084a5d
João Victor da Silva <jv782063@gmail.com>
2024-04-22 12:26:20 -0300

    remove tview

--- go.mod
+++ go.mod
@@ -1,16 +1,3 @@
 module github.com/joaovds/go-2048
 
 go 1.22.2
-
-require github.com/rivo/tview v0.0.0-20240420134618-e119d15762fe
-
-require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.7.1 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/term v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-)
--- internal/ui/layout.go
+++ internal/ui/layout.go
@@ -1,31 +1,14 @@
 package ui
 
-import (
-	"github.com/rivo/tview"
-)
 
 type Layout struct {
-	BoardComponent   *Board
-	DetailsComponent *Details
 }
 
 func NewLayout() *Layout {
 	return &Layout{
-		BoardComponent:   NewBoard(),
-		DetailsComponent: NewDetails(),
 	}
 }
 
 func (l *Layout) Render() {
-	layout := tview.NewGrid().
-		SetRows(0).
-		SetColumns(-2, 0).
-		SetBorders(true).
-		AddItem(l.BoardComponent.Render(), 0, 0, 1, 1, 0, 0, false).
-		AddItem(l.DetailsComponent.Render(), 0, 1, 1, 1, 0, 0, false)
-
-	if err := tview.NewApplication().SetRoot(layout, true).
-		SetFocus(layout).Run(); err != nil {
-		panic(err)
-	}
+	println("Rendering layout")
 }
